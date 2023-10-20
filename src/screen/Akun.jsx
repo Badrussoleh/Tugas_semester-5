3fb2dceea6a1af1b7950c18646d3a00bdb876bde
waid waid <jironfaid13@gmail.com>
2023-10-20 10:37:20 +0700

    ini tugas saya bapak

---
+++ src/screen/Akun.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, Text, Alert, TouchableOpacity} from 'react-native'
+import Tombol from '../component/Tombol';
+
+export default class Akun extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={styles.judul}>
+                    <Text style={{color: 'cornflowerblue', fontStyle: 'normal', fontWeight: 'bold'}}>Hiii </Text>
+                    Badrus</Text>
+                <TouchableOpacity 
+                onPress={() => this.props.navigation.navigate('Beranda')}>
+                    <Text>Ke Beranda</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    };
+}
+
+const styles = StyleSheet.create({
+    judul: {
+        color: 'black',
+        fontSize: 20,
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+        textAlign: 'left',
+        marginTop: 5,
+        backgroundColor: 'mintcream',
+        elevation: 20,
+        paddingBottom: 10,
+        paddingTop: 10,
+        paddingLeft: 5,
+        marginBottom: 10,
+    },
+})
---
+++ src/screen/Beranda.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, Text, Alert, TouchableOpacity, } from 'react-native'
+import Tombol from '../component/Tombol';
+
+export default class Beranda extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={styles.judul}>
+                    <Text style={{color: 'cornflowerblue', fontStyle: 'normal', fontWeight: 'bold'}}>Mari</Text>
+                    Belajar</Text>
+                <TouchableOpacity 
+                onPress={() => this.props.navigation.navigate('Akun')}>
+                    <Text>Ke Akun</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    };
+}
+
+const styles = StyleSheet.create({
+    judul: {
+        color: 'dimgray',
+        fontSize: 20,
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+        textAlign: 'right',
+        marginTop: 5,
+        backgroundColor: 'mintcream',
+        elevation: 20,
+        paddingBottom: 10,
+        paddingTop: 10,
+        paddingRight: 5,
+        marginBottom: 10,
+    },
+})
