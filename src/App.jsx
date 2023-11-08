9b8616ad58c280a62ffa877d8898949e4c69cba5
waid waid <jironfaid13@gmail.com>
2023-11-08 08:36:37 +0700

    Update pertama

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,70 @@ import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Beranda from './screen/Beranda';
 import Akun from './screen/Akun';
+import Card from './screen/Card';
+import History from './screen/History';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Beranda"
+      options={{
+        headerShown: false, tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+      component={Beranda} />
+      <Tab.Screen name="Akun" 
+      options={{
+        headerShown: false, tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="account" color={color} size={size} />
+        ),
+      }}
+      component={Akun} />
+      <Tab.Screen name="Card"
+      options={{
+        headerShown: false, tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="credit-card" color={color} size={size} />
+        ),
+      }}
+      component={Card} />
+      <Tab.Screen name="History"
+      options={{
+        headerShown: false, tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="clock-rotate-left" color={color} size={size} />
+        ),
+      }}
+      component={History} />
+    </Tab.Navigator>
+  );
+};
 
 function App ()  {
   return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Beranda">
           <Stack.Screen 
-            name="Beranda" 
+            name="Main" 
             options={{headerShown: false}}
-            component={Beranda} 
+            component={Tabs} 
           />
           <Stack.Screen 
           name="Akun" 
           options={{headerShown: false}}
           component={Akun} 
+          />
+          <Stack.Screen 
+          name="Card" 
+          options={{headerShown: false}}
+          component={Card} 
+          />
+          <Stack.Screen 
+          name="History" 
+          options={{headerShown: false}}
+          component={History} 
           />
         </Stack.Navigator>
       </NavigationContainer>
